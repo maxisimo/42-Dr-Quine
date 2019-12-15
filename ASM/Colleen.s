fdeca4b1d8b1c0204bd1fae234f4e3838d32d6f8
maxisimo <34480775+maxisimo@users.noreply.github.com>
2019-12-15 11:49:34 +0100

    Update Colleen.s

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,21 +1,27 @@
-; commentaire
+; Un commentaire
+
 section .data
 hello:
-.string db "; commentaire%2$csection .data%2$chello:%2$c.string db %3$c%1$s%3$c, 0%2$csection .text%2$cglobal start%2$cglobal _main%2$cextern _printf%2$cstart:%2$ccall _main%2$cret%2$c_main:%2$cpush rbp; commentaire%2$cmov rbp, rsp%2$clea rdi, [rel hello.string]%2$clea rsi, [rel hello.string]%2$cmov rdx, 10%2$cmov rcx, 34%2$ccall _printf%2$cleave%2$cret%2$c", 0
+	.string db "; Un commentaire%2$c%2$csection .data%2$chello:%2$c%4$c.string db %3$c%1$s%3$c, 0%2$c%2$csection .text%2$c%4$cglobal start%2$%4$ccglobal _main%2$%4$ccextern _printf%2$c%2$cstart:%2$c%4$ccall _main%2$c%4$cret%2$c%2$c_main:%2$c%4$c; Un commentaire%2$c%4$cpush rbp%2$c%4$cmov rbp, rsp%2$c%4$clea rdi, [rel hello.string]%2$c%4$clea rsi, [rel hello.string]%2$c%4$cmov rdx, 10%2$cmov rcx, 34%2$%4$cmov r8, 9%2$c%4$cccall _printf%2$c%4$cleave%2$c%4$cret%2$c", 0
+
 section .text
-global start
-global _main
-extern _printf
+	global start
+	global _main
+	extern _printf
+
 start:
-call _main
-ret
+	call _main
+	ret
+
 _main:
-push rbp; commentaire
-mov rbp, rsp
-lea rdi, [rel hello.string]
-lea rsi, [rel hello.string]
-mov rdx, 10
-mov rcx, 34
-call _printf
-leave
-ret
+	; Un commentaire
+	push rbp
+	mov rbp, rsp
+	lea rdi, [rel hello.string]
+	lea rsi, [rel hello.string]
+	mov rdx, 10
+	mov rcx, 34
+	mov r8, 9
+	call _printf
+	leave
+	ret
