0fd3e20a25ea9fc5587beb7eb989c36aee1751c0
Maxime <maxime.simon42@gmail.com>
2019-12-28 15:16:22 +0100

    improve Makefile

--- C/Colleen.c
+++ C/Colleen.c
@@ -13,9 +13,10 @@ void	function(char *s) {
 int		main(void) {
 	char	*s;
 
-	s = "#include <stdio.h>%2$c#define TAB 9%2$c#define NL 10%2$c#define DQUOTE 34%2$c/*%2$c%1$cUn commentaire%2$c*/%2$c%2$cvoid%1$cfunction(char *s) {%2$c%1$cprintf(s, TAB, NL, DQUOTE, s);%2$c}%2$c%2$cint%1$c%1$cmain(void) {%2$c%1$cchar%1$c*s;%2$c%2$c%1$cs = %3$c%4$s%3$c;%2$c%1$c/*%2$c%1$c%1$cUn commentaire%2$c%1$c*/%2$c%1$cfunction(s);%2$c}%2$c";
+	s = "#include <stdio.h>%2$c#define TAB 9%2$c#define NL 10%2$c#define DQUOTE 34%2$c/*%2$c%1$cUn commentaire%2$c*/%2$c%2$cvoid%1$cfunction(char *s) {%2$c%1$cprintf(s, TAB, NL, DQUOTE, s);%2$c}%2$c%2$cint%1$c%1$cmain(void) {%2$c%1$cchar%1$c*s;%2$c%2$c%1$cs = %3$c%4$s%3$c;%2$c%1$c/*%2$c%1$c%1$cUn commentaire%2$c%1$c*/%2$c%1$cfunction(s);%2$c%1$creturn 0;%2$c}%2$c";
 	/*
 		Un commentaire
 	*/
 	function(s);
+	return 0;
 }
