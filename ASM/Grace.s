f6f06fb81982c7d4df54e028d885724ffae1c795
Maxime <maxime.simon42@gmail.com>
2020-01-28 10:37:33 +0100

    more more tests

--- ASM/Grace.s
+++ ASM/Grace.s
@@ -1,16 +1,18 @@
-; Un commentaire
-
-%define STRING "; Un commentaire%2$c%%define STRING %3$c%1$s%3$c%2$c%%define CHILD %3%cGrace_kid.s%3$c%2$c%2$csection .data%2$chello:%2$c%4$c.string db STRING, 0%2$c%4$c.file db CHILD, 0%2$c%2$c%%macro start 1%2$c%2$cextern _main, _dprintf%2$c%2$c_main:%2$c%4$cpush rpb%2$c%4$cmovrbp, rsp%2$c%4$clea rdi, [rel hello.file]%2$c%4$cmov rsi, 0x0202%2$c%4$cmov rax, 0x2000005%2$c%4$csyscall%2$c%4$cmov rdi, rax%2$c%4$clea rsi, [rel hello.string]%2$c%4$clea rdx, [rel hello.string]%2$c%4$cmov rcx, 10%2$c%4$cmov r8, 34%2$c%4$cmov r9, 9%2$c%4$ccall _dprintf%2$c%4$cleave%2$c%4$cret%2$c%4$c%2$c%%endmacro%2$c%2$cstart STRING%2$c"
-%define CHILD "Grace_kid.s"
+;commentaire bitch
+%define STR "Ceci est un test"
+%define DECLARE_MAIN global _main
 
 section .data
 hello:
-	.string db STRING, 0
+	.str db STR, 0
 	.file db CHILD, 0
+
+section .text
+	extern _dprintf
 
 %macro start 1
 
-extern _main, _fprintf
+extern _main
 
 _main:
 	push rbp
@@ -19,16 +21,15 @@ _main:
 	mov rsi, 0x0202
 	mov rax, 0x2000005
 	syscall
+	
 	mov rdi, rax
-	lea rsi, [rel hello.string]
-	lea rdx, [rel hello.string]
-	mov rcx, 10
-	mov r8, 34
-	mov r9, 9
-	call _fprintf
+	lea rsi, [rel hello.str]
+	lea rdx, [rel hello.str]
+	mov rcx, 9
+	mov r8, 10
+	mov r9, 34
+	call _dprintf
 	leave
 	ret
 
 %endmacro
-
-start STRING
--- ASM/Sully.s
+++ ASM/Sully.s
@@ -1,18 +1,100 @@
 section .data
-start:
-	i equ 5
-	.string db "", 0
-	section .text
+check_file:
+	db "Sully_5.s", 0
+file_name:
+	db "Sully_%d.%c", 0
+output_name:
+	db "Sully_%d", 0
+command:
+	db "nasm -f macho64 %2$s && gcc -o %1$s %3$s && ./%1$s", 0
+file_content:
+	db "Ceci est un test", 0
+
+section .bss
+final_cmd:  resb    255
+path:       resb    15
+output:     resb    15
+object:     resb    15
+file_num:   resb    4
+
+section .text
 	global _main
-	extern _printf
+	extern _sprintf
+	extern _dprintf
+	extern _system
+	extern _access
 
 _main:
-	push rbp
-	mov rbp, rsp
-	lea rdi, [rel start.string]
-	lea rsi, [rel start.string]
-	mov rdx, 10
-	mov rcx, 34
-	call _printf
-	leave
-	ret
+    push    rbx
+    mov     rdi, 5
+    mov     [rel file_num], rdi
+    lea     rdi, [rel check_file]
+    mov     rsi, 0x04
+    call    _access
+    cmp     rax, -1
+    je     create_path
+    mov     rdi, [rel file_num]
+    dec     rdi
+    mov     [rel file_num], rdi
+
+create_path:
+    lea     rdi, [rel path]
+    lea     rsi, [rel file_name]
+    mov     rdx, [rel file_num]
+    mov     rcx, 's'
+    call    _sprintf
+
+create_object:
+    lea     rdi, [rel object]
+    lea     rsi, [rel file_name]
+    mov     rdx, [rel file_num]
+    mov     rcx, 'o'
+    call    _sprintf
+
+create_output:
+    lea     rdi, [rel output]
+    lea     rsi, [rel output_name]
+    mov     rdx, [rel file_num]
+    call    _sprintf
+
+create_command:
+    lea     rdi, [rel final_cmd]
+    lea     rsi, [rel command]
+    lea     rdx, [rel output]
+    lea     rcx, [rel path]
+    lea     r8, [rel object]
+    call    _sprintf
+
+create_file:
+    lea rdi, [rel path]
+    mov rsi, 0x202
+    mov rax, 0x2000005
+    mov rdx, 644o
+    syscall
+    mov     rdi, rax
+    lea     rsi, [rel file_content]
+    mov     rdx, 10
+    mov     rcx, 34
+    lea     r8, [rel file_content]
+    mov     r9, [rel file_num]
+    lea     rbx, [rel file_name]
+    push    rbx
+    lea     rbx, [rel output_name]
+    push    rbx
+    lea     rbx, [rel command]
+    push    rbx
+    push    rbx
+    call    _dprintf
+    pop     rbx
+    pop     rbx
+    pop     rbx
+    pop     rbx
+    mov     r12, [rel file_num]
+    cmp     r12, 0
+    jle      exit
+    lea     rdi, [rel final_cmd]
+    call    _system
+
+exit:
+    pop     rbx
+    ret
