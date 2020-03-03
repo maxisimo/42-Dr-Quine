1db3a55373427b090e7fc6368af040aefc8056d3
Maxime SIMON <maxisimo@e1r5p18.42.fr>
2020-03-03 19:07:08 +0100

    test with printf & dprintf

--- ASM/test.s
+++ ASM/test.s
@@ -1,30 +1,24 @@
 section .data
 check_file:
-	db "Sully_5.s"
+	db "Sully_5.s", 0
 file_name:
 	db "Sully_%d.%c", 0
-file_exec:
-	db "Sully_%d", 0
-command:
-	db "nasm -f macho64 %1$s -o %2$s && gcc %2$s -o %3$s && ./%3$s", 0
-file_content:
-	db "section .data%1$ccheck_file:%1$c%3$cdb %2$cSully_5.s%2$c, 0%1$cfile_name:%1$c%3$cdb %2$cSully_%%d.%%c%2$c, 0%1$cfile_exec:%1$c%3$cdb %2$cSully_%%d%2$c, 0%1$ccommand:%1$c%3$cdb %2$cnasm -f macho64 %%1$s -o %%2$s && gcc %%2$s -o %%3$s && ./%%3$s%2$c, 0%1$cfile_content:%1$c%3$cdb %2$c%4$s%2$c, 0%1$c%1$csection .bss%1$cfinal_cmd:%3$cresb 255%1$cpath:%3$c%3$cresb 15%1$cobject:%3$c%3$cresb 15%1$coutput:%3$c%3$cresb 15%1$cfile_num:%3$cresb 2%1$c%1$csection .text%1$c%3$cglobal _main%1$c%3$cextern _dprintf%1$c%3$cextern _sprintf%1$c%3$cextern _system%1$c%1$c_main:%1$c%3$cpush rbp,%1$c%3$cmov rbp, rsp%1$c%3$cmov rdi, %5$d%1$c%3$cmov [rel file_num], rdi%1$c%3$clea rdi, [rel check_file]%1$c%3$cmov rsi, 0x04%1$c%3$ccall _access%1$c%3$ccmp rax, -1%1$c%3$cje create_path%1$c%3$cmov rdi, [rel file_num]%1$c%3$cdec rdi%1$c%3$cmov [rel file_num], rdi%1$c%1$ccreate_path:%1$c%3$clea rdi, [rel path]%1$c%3$clea rsi, [rel file_name]%1$c%3$cmov rdx, [rel file_num]%1$c%3$cmov rcx, 's'%1$c%3$ccall _sprintf%1$c%1$ccreate_object:%1$c%3$clea rdi, [rel object]%1$c%3$clea rsi, [rel file_name]%1$c%3$cmov rdx, [rel file_num]%1$c%3$cmov rcx, 'o'%1$c%3$ccall _sprintf%1$c%1$ccreate_output:%1$c%3$clea rdi, [rel output]%1$c%3$clea rsi, [rel file_name]%1$c%3$cmov rdx, [rel file_num]%1$c%3$ccall _sprintf%1$c%1$ccreate_command:%1$c%3$clea rdi, [rel final_cmd]%1$c%3$clea rsi, [command]%1$c%3$cmov rdx, [rel path]%1$c%3$cmov rcx, [rel object]%1$c%3$cmov r8, [rel output]%1$c%3$ccall _sprintf%1$c%1$ccreate_file:%1$c%3$cmov rax, 0x2000005%1$c%3$clea rdi, [rel path]%1$c%3$cmov rsi, 0x202%1$c%3$cmov rdx, 0o644%1$c%3$csyscall%1$c%1$c%3$cmov rdi, rax%1$c%3$clea rsi, [rel file_content]%1$c%3$clea rdx, [rel file_content]%1$c%3$cmov rcx, 9%1$c%3$cmov r8, 10%1$c%3$cmov r9, 34%1$c%3$cmov r10, [rel file_num]%1$c%3$clea rbx, [rel file_name]%1$c%3$cpush rbx%1$c%3$clea rbx, [rel file_exec]%1$c%3$cpush rbx%1$c%3$cpush rbx%1$c%3$clea rbx, [rel final_cmd]%1$c%3$cpush rbx%1$c%3$ccall _dprintf%1$c%3$cpop rbx%1$c%3$cpop rbx%1$c%3$cpop rbx%1$c%3$cpop rbx%1$c%3$cmov r12, [rel file_num]%1$c%3$ccmp r12, 0%1$c%3$cjle exit%1$c%3$clea rdi, [rel final_cmd]%1$c%3$ccall _system%1$c%1$cexit:%1$c%3$cpop rbx%1$c%3$cret%1$c%1$c", 0
+hello:
+	.string db "J'ai %5$d doigts", 0
 
 section .bss
-final_cmd:	resb 255
 path:		resb 15
-object:		resb 15
-output:		resb 15
-file_num:	resb 2
+file_num:	resb 4
 
 section .text
 	global _main
-	extern _dprintf
+	extern _printf
 	extern _sprintf
-	extern _system
+	extern _dprintf
+	extern _access
 
 _main:
-	push rbp,
+	push rbp
 	mov rbp, rsp
 	mov rdi, 5
 	mov [rel file_num], rdi
@@ -44,62 +38,32 @@ create_path:
 	mov rcx, 's'
 	call _sprintf
 
-create_object:
-	lea rdi, [rel object]
-	lea rsi, [rel file_name]
-	mov rdx, [rel file_num]
-	mov rcx, 'o'
-	call _sprintf
-
-create_output:
-	lea rdi, [rel output]
-	lea rsi, [rel file_name]
-	mov rdx, [rel file_num]
-	call _sprintf
-
-create_command:
-	lea rdi, [rel final_cmd]
-	lea rsi, [command]
-	mov rdx, [rel path]
-	mov rcx, [rel object]
-	mov r8, [rel output]
-	call _sprintf
-
-create_file:
-	mov rax, 0x2000005
+print_string:
 	lea rdi, [rel path]
-	mov rsi, 0x202
+	mov rax, 0x2000005
 	mov rdx, 0o644
+	mov rsi, 0x0202
 	syscall
 
 	mov rdi, rax
-	lea rsi, [rel file_content]
-	lea rdx, [rel file_content]
-	mov rcx, 9
-	mov r8, 10
-	mov r9, 34
+	lea rsi, [rel hello.string]
+	lea rdx, [rel hello.string]
+	mov rcx, 10
+	mov r8, 34
+	mov r9, 9
 	mov r10, [rel file_num]
-	lea rbx, [rel file_name]
-	push rbx
-	lea rbx, [rel file_exec]
-	push rbx
-	push rbx
-	lea rbx, [rel final_cmd]
-	push rbx
 	call _dprintf
-	pop rbx
-	pop rbx
-	pop rbx
-	pop rbx
-	mov r12, [rel file_num]
-	cmp r12, 0
-	jle exit
-	lea rdi, [rel final_cmd]
-	call _system
 
-exit:
-	pop rbx
+	lea rdi, [rel hello.string]
+	lea rsi, [rel hello.string]
+	mov rdx, 10
+	mov rcx, 34
+	mov r8, 9
+	mov r9, [rel file_num]
+	call _printf
+	leave
 	ret
+
 
 ; https://www.unilim.fr/pages_perso/tristan.vaccon/cours_nasm.pdf
 ; http://www.lacl.fr/tan/asm
