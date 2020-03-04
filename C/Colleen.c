da1744964a1bc00cbddcf6f7a29e4a1c335cb5f7
Maxime SIMON <maxisimo@e1r4p20.42.fr>
2020-03-04 19:55:46 +0100

    Finalize .c

--- C/Colleen.c
+++ C/Colleen.c
@@ -3,7 +3,7 @@
 #define NL 10
 #define DQUOTE 34
 /*
-	Un commentaire
+	This program will print its own source
 */
 
 void	function(char *s) {
@@ -13,9 +13,9 @@ void	function(char *s) {
 int		main(void) {
 	char	*s;
 
-	s = "#include <stdio.h>%2$c#define TAB 9%2$c#define NL 10%2$c#define DQUOTE 34%2$c/*%2$c%1$cUn commentaire%2$c*/%2$c%2$cvoid%1$cfunction(char *s) {%2$c%1$cprintf(s, TAB, NL, DQUOTE, s);%2$c}%2$c%2$cint%1$c%1$cmain(void) {%2$c%1$cchar%1$c*s;%2$c%2$c%1$cs = %3$c%4$s%3$c;%2$c%1$c/*%2$c%1$c%1$cUn commentaire%2$c%1$c*/%2$c%1$cfunction(s);%2$c%1$creturn 0;%2$c}%2$c";
+	s = "#include <stdio.h>%2$c#define TAB 9%2$c#define NL 10%2$c#define DQUOTE 34%2$c/*%2$c%1$cThis program will print its own source%2$c*/%2$c%2$cvoid%1$cfunction(char *s) {%2$c%1$cprintf(s, TAB, NL, DQUOTE, s);%2$c}%2$c%2$cint%1$c%1$cmain(void) {%2$c%1$cchar%1$c*s;%2$c%2$c%1$cs = %3$c%4$s%3$c;%2$c%1$c/*%2$c%1$c%1$cAnother comment%2$c%1$c*/%2$c%1$cfunction(s);%2$c%1$creturn 0;%2$c}%2$c";
 	/*
-		Un commentaire
+		Another comment
 	*/
 	function(s);
 	return 0;
--- C/Grace.c
+++ C/Grace.c
@@ -8,9 +8,9 @@ void	grace() {
 	char	path[30];
 	FILE	*child;
 
-	s = "#include <stdio.h>%2$c#define TAB 9%2$c#define NL 10%2$c#define MAIN int main(void) { grace(); }%2$c%2$cvoid%1$cgrace() {%2$c%1$cchar%1$c*s;%2$c%1$cchar%1$cpath[30];%2$c%1$cFILE%1$c*child;%2$c%2$c%1$cs = %3$c%4$s%3$c;%2$c%1$c/*%2$c%1$c%1$cUn commentaire%2$c%1$c*/%2$c%1$csprintf(path, %3$cGrace_kid.c%3$c);%2$c%1$cchild = fopen(path, %3$cw%3$c);%2$c%1$cif (child > 0)%2$c%1$c%1$cfprintf(child, s, TAB, NL, 34, s);%2$c}%2$c%2$cMAIN%2$c";
+	s = "#include <stdio.h>%2$c#define TAB 9%2$c#define NL 10%2$c#define MAIN int main(void) { grace(); }%2$c%2$cvoid%1$cgrace() {%2$c%1$cchar%1$c*s;%2$c%1$cchar%1$cpath[30];%2$c%1$cFILE%1$c*child;%2$c%2$c%1$cs = %3$c%4$s%3$c;%2$c%1$c/*%2$c%1$c%1$cThis program will print its own source in a file named Grace_kid.s%2$c%1$c*/%2$c%1$csprintf(path, %3$cGrace_kid.c%3$c);%2$c%1$cchild = fopen(path, %3$cw%3$c);%2$c%1$cif (child > 0)%2$c%1$c%1$cfprintf(child, s, TAB, NL, 34, s);%2$c}%2$c%2$cMAIN%2$c";
 	/*
-		Un commentaire
+		This program will print its own source in a file named Grace_kid.s
 	*/
 	sprintf(path, "Grace_kid.c");
 	child = fopen(path, "w");
