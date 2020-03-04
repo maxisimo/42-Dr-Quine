815fb68e16fac3ef92d2996922c37436e52697c2
Maxime SIMON <maxisimo@e1r4p20.42.fr>
2020-03-04 19:51:55 +0100

    Finalize program

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,8 +1,8 @@
-; Un commentaire
+; This program will print its own source
 
 section .data
-hello:
-	.string db "; Un commentaire%2$c%2$csection .data%2$chello:%2$c%4$c.string db %3$c%1$s%3$c, 0%2$c%2$csection .text%2$c%4$cglobal start%2$%4$ccglobal _main%2$%4$ccextern _printf%2$c%2$cstart:%2$c%4$ccall _main%2$c%4$cret%2$c%2$c_main:%2$c%4$c; Un commentaire%2$c%4$cpush rbp%2$c%4$cmov rbp, rsp%2$c%4$clea rdi, [rel hello.string]%2$c%4$clea rsi, [rel hello.string]%2$c%4$cmov rdx, 10%2$cmov rcx, 34%2$%4$cmov r8, 9%2$c%4$cccall _printf%2$c%4$cleave%2$c%4$cret%2$c", 0
+file_content:
+	db "; This program will print its own source%2$c%2$csection .data%2$cfile_content:%2$c%4$cdb %3$c%1$s%3$c, 0%2$c%2$csection .text%2$c%4$cglobal start%2$c%4$cglobal _main%2$c%4$cextern _printf%2$c%2$cstart:%2$c%4$ccall _main%2$c%4$cret%2$c%2$c_main:%2$c%4$c; One comment%2$c%4$cpush rbp%2$c%4$cmov rbp, rsp%2$c%4$clea rdi, [rel file_content]%2$c%4$clea rsi, [rel file_content]%2$c%4$cmov rdx, 10%2$c%4$cmov rcx, 34%2$c%4$cmov r8, 9%2$c%4$ccall _printf%2$c%4$cleave%2$c%4$cret%2$c", 0
 
 section .text
 	global start
@@ -14,11 +14,11 @@ start:
 	ret
 
 _main:
-	; Un commentaire
+	; One comment
 	push rbp
 	mov rbp, rsp
-	lea rdi, [rel hello.string]
-	lea rsi, [rel hello.string]
+	lea rdi, [rel file_content]
+	lea rsi, [rel file_content]
 	mov rdx, 10
 	mov rcx, 34
 	mov r8, 9
--- ASM/Grace.s
+++ ASM/Grace.s
@@ -1,12 +1,13 @@
-; Un commentaire
+; This program will print its own source in a file named Grace_kid.s
 
-%define STRING "; Un commentaire%3$c%3$c%%define STRING %4$c%1$s%4$c%3$c%%define CHILD %4$cGrace_kid.s%4$c%3$c%3$csection .data%3$chello:%3$c%2$c.str db STRING, 0%3$c%2$c.filename db CHILD, 0%3$c%3$csection .text%3$c%2$cextern _dprintf%3$c%3$c%%macro start 1%3$c%3$cextern _main%3$c%3$c_main:%3$c%2$cpush rbp%3$c%2$cmov rbp, rsp%3$c%2$cmov rax, 0x2000005%3$c%2$clea rdi, [rel hello.filename]%3$c%2$cmov rsi, 0x0202%3$c%2$cmov rdx, 0o644%3$c%2$csyscall%3$c%3$c%2$cmov rdi, rax%3$c%2$clea rsi, [rel hello.str]%3$c%2$clea rdx, [rel hello.str]%3$c%2$cmov rcx, 9%3$c%2$cmov r8, 10%3$c%2$cmov r9, 34%3$c%2$ccall _dprintf%3$c%2$cleave%3$c%2$cret%3$c%3$c%%endmacro%3$c%3$cstart STRING%3$c"
+%define STRING "; This program will print its own source in a file named Grace_kid.s%3$c%3$c%%define STRING %4$c%1$s%4$c%3$c%%define CHILD %4$cGrace_kid.s%4$c%3$c%3$csection .data%3$cfile_name:%3$c%2$cdb CHILD, 0%3$cfile_content:%3$c%2$cdb STRING, 0%3$c%3$csection .text%3$c%2$cextern _dprintf%3$c%3$c%%macro start 1%3$c%3$cextern _main%3$c%3$c_main:%3$c%2$cpush rbp%3$c%2$cmov rbp, rsp%3$c%2$clea rdi, [rel file_name]%3$c%2$cmov rax, 0x2000005%3$c%2$cmov rdx, 0o644%3$c%2$cmov rsi, 0x0202%3$c%2$csyscall%3$c%3$c%2$cmov rdi, rax%3$c%2$clea rsi, [rel file_content]%3$c%2$clea rdx, [rel file_content]%3$c%2$cmov rcx, 9%3$c%2$cmov r8, 10%3$c%2$cmov r9, 34%3$c%2$ccall _dprintf%3$c%2$cleave%3$c%2$cret%3$c%3$c%%endmacro%3$c%3$cstart STRING%3$c"
 %define CHILD "Grace_kid.s"
 
 section .data
-hello:
-	.str db STRING, 0
-	.filename db CHILD, 0
+file_name:
+	db CHILD, 0
+file_content:
+	db STRING, 0
 
 section .text
 	extern _dprintf
@@ -18,15 +19,15 @@ extern _main
 _main:
 	push rbp
 	mov rbp, rsp
+	lea rdi, [rel file_name]
 	mov rax, 0x2000005
-	lea rdi, [rel hello.filename]
-	mov rsi, 0x0202
 	mov rdx, 0o644
+	mov rsi, 0x0202
 	syscall
 
 	mov rdi, rax
-	lea rsi, [rel hello.str]
-	lea rdx, [rel hello.str]
+	lea rsi, [rel file_content]
+	lea rdx, [rel file_content]
 	mov rcx, 9
 	mov r8, 10
 	mov r9, 34
