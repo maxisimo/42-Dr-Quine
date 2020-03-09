9944fabd48739db25805c81e61fafdbc304ff10c
Maxime SIMON <maxisimo@e1r4p12.42.fr>
2020-03-09 11:01:15 +0100

    Do not have any declared functions any more

--- C/Grace.c
+++ C/Grace.c
@@ -1,21 +1,9 @@
 #include <stdio.h>
-#define TAB 9
-#define NL 10
-#define MAIN int main(void) { grace(); }
-
-void	grace() {
-	char	*s;
-	char	path[30];
-	FILE	*child;
-
-	s = "#include <stdio.h>%2$c#define TAB 9%2$c#define NL 10%2$c#define MAIN int main(void) { grace(); }%2$c%2$cvoid%1$cgrace() {%2$c%1$cchar%1$c*s;%2$c%1$cchar%1$cpath[30];%2$c%1$cFILE%1$c*child;%2$c%2$c%1$cs = %3$c%4$s%3$c;%2$c%1$c/*%2$c%1$c%1$cThis program will print its own source in a file named Grace_kid.s%2$c%1$c*/%2$c%1$csprintf(path, %3$cGrace_kid.c%3$c);%2$c%1$cchild = fopen(path, %3$cw%3$c);%2$c%1$cif (child > 0)%2$c%1$c%1$cfprintf(child, s, TAB, NL, 34, s);%2$c}%2$c%2$cMAIN%2$c";
-	/*
-		This program will print its own source in a file named Grace_kid.s
-	*/
-	sprintf(path, "Grace_kid.c");
-	child = fopen(path, "w");
-	if (child > 0)
-		fprintf(child, s, TAB, NL, 34, s);
-}
+#define CHILD "Grace_kid.c"
+#define STRING "#include <stdio.h>%2$c#define CHILD %3$cGrace_kid.c%3$c%2$c#define STRING %3$c%4$s%3$c%2$c#define MAIN int main(void) { char *s; char path[30]; FILE *child; s = STRING; sprintf(path, CHILD); child = fopen(path, %3$cw%3$c); if (child > 0) fprintf(child, s, 9, 10, 34, s); }%2$c/*%2$c%1$cThis program will print its own source in a file named Grace_kid.s%2$c*/%2$c%2$cMAIN%2$c"
+#define MAIN int main(void) { char *s; char path[30]; FILE *child; s = STRING; sprintf(path, CHILD); child = fopen(path, "w"); if (child > 0) fprintf(child, s, 9, 10, 34, s); }
+/*
+	This program will print its own source in a file named Grace_kid.s
+*/
 
 MAIN
