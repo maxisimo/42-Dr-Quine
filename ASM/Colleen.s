9b61cf691512a7c0be44d607a289de98547bd688
Maxime SIMON <maxisimo@e1r5p12.42.fr>
2020-09-14 18:59:21 +0200

    improve

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -2,10 +2,9 @@
 
 section .data
 file_content:
-	db "; This program will print its own source%2$c%2$csection .data%2$cfile_content:%2$c%4$cdb %3$c%1$s%3$c, 0%2$c%2$csection .text%2$c%4$cglobal start%2$c%4$cglobal _main%2$c%4$cextern _printf%2$c%2$cstart:%2$c%4$ccall _main%2$c%4$cret%2$c%2$c_main:%2$c%4$c; One comment%2$c%4$cpush rbp%2$c%4$cmov rbp, rsp%2$c%4$clea rdi, [rel file_content]%2$c%4$clea rsi, [rel file_content]%2$c%4$cmov rdx, 10%2$c%4$cmov rcx, 34%2$c%4$cmov r8, 9%2$c%4$ccall _printf%2$c%4$cleave%2$c%4$cret%2$c", 0
+	db "; This program will print its own source%2$c%2$csection .data%2$cfile_content:%2$c%4$cdb %3$c%1$s%3$c, 0%2$c%2$csection .text%2$c%4$cglobal _main%2$c%4$cextern _printf%2$c%2$cstart:%2$c%4$ccall _main%2$c%4$cret%2$c%2$c_main:%2$c%4$c; One comment%2$c%4$cpush rbp%2$c%4$cmov rbp, rsp%2$c%4$clea rdi, [rel file_content]%2$c%4$clea rsi, [rel file_content]%2$c%4$cmov rdx, 10%2$c%4$cmov rcx, 34%2$c%4$cmov r8, 9%2$c%4$ccall _printf%2$c%4$cmov rsp, rbp%2$c%4$cpop rbp%2$c%4$cret%2$c", 0
 
 section .text
-	global start
 	global _main
 	extern _printf
 
@@ -23,5 +22,6 @@ _main:
 	mov rcx, 34
 	mov r8, 9
 	call _printf
-	leave
+	mov rsp, rbp
+	pop rbp
 	ret
