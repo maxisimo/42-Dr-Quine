ca56ca5b57ad8cf0c1e490eb48a2b5af1145beee
Maxime SIMON <maxisimo@e1r4p22.42.fr>
2020-09-15 16:07:34 +0200

    great maj

--- ASM/Grace.s
+++ ASM/Grace.s
@@ -1,6 +1,6 @@
 ; This program will print its own source in a file named Grace_kid.s
 
-%define STRING "; This program will print its own source in a file named Grace_kid.s%3$c%3$c%%define STRING %4$c%1$s%4$c%3$c%%define CHILD %4$cGrace_kid.s%4$c%3$c%3$csection .data%3$cfile_name:%3$c%2$cdb CHILD, 0%3$cfile_content:%3$c%2$cdb STRING, 0%3$c%3$csection .text%3$c%2$cextern _dprintf%3$c%3$c%%macro start 1%3$c%3$cextern _main%3$c%3$c_main:%3$c%2$cpush rbp%3$c%2$cmov rbp, rsp%3$c%2$clea rdi, [rel file_name]%3$c%2$cmov rax, 0x2000005%3$c%2$cmov rdx, 0o644%3$c%2$cmov rsi, 0x0202%3$c%2$csyscall%3$c%3$c%2$cmov rdi, rax%3$c%2$clea rsi, [rel file_content]%3$c%2$clea rdx, [rel file_content]%3$c%2$cmov rcx, 9%3$c%2$cmov r8, 10%3$c%2$cmov r9, 34%3$c%2$ccall _dprintf%3$c%2$cleave%3$c%2$cret%3$c%3$c%%endmacro%3$c%3$cstart STRING%3$c"
+%define STRING "; This program will print its own source in a file named Grace_kid.s%3$c%3$c%%define STRING %4$c%1$s%4$c%3$c%%define CHILD %4$cGrace_kid.s%4$c%3$c%3$csection .data%3$cfile_name:%3$c%2$cdb CHILD, 0%3$cfile_content:%3$c%2$cdb STRING, 0%3$c%3$csection .text%3$c%2$cextern _dprintf%3$c%3$c%%macro start 1%3$c%3$cextern _main%3$c%3$c_main:%3$c%2$cpush rbp%3$c%2$cmov rbp, rsp%3$c%2$clea rdi, [rel file_name]%3$c%2$cmov rax, 0x2000005%3$c%2$cmov rdx, 0o644%3$c%2$cmov rsi, 0x0202%3$c%2$csyscall%3$c%3$c%2$cmov rdi, rax%3$c%2$clea rsi, [rel file_content]%3$c%2$clea rdx, [rel file_content]%3$c%2$cmov rcx, 9%3$c%2$cmov r8, 10%3$c%2$cmov r9, 34%3$c%2$ccall _dprintf%3$c%2$cmov rsp, rbp%3$c%2$cpop rbp%3$c%2$cret%3$c%3$c%%endmacro%3$c%3$cstart STRING%3$c"
 %define CHILD "Grace_kid.s"
 
 section .data
@@ -32,7 +32,8 @@ _main:
 	mov r8, 10
 	mov r9, 34
 	call _dprintf
-	leave
+	mov rsp, rbp
+	pop rbp
 	ret
 
 %endmacro
--- ASM/Sully.s
+++ ASM/Sully.s
@@ -6,12 +6,15 @@ file_name:
 output_name:
 	db "Sully_%d", 0
 command:
-	db "nasm -f macho64 -D VAR %2$s && gcc -o %1$s %3$s && ./%1$s", 0
+	db "nasm -f macho64 -D VAR %2$s && gcc -o %1$s %3$s", 0
+execute:
+	db "./%1$s", 0
 file_content:
-	db "section .data%1$ccheck_file:%1$c	db %2$cSully_5.s%2$c, 0%1$cfile_name:%1$c	db %2$c%8$s%2$c, 0%1$coutput_name:%1$c	db %2$c%7$s%2$c, 0%1$ccommand:%1$c	db %2$c%5$s%2$c, 0%1$cfile_content:%1$c	db %2$c%3$s%2$c, 0%1$c%1$csection .bss%1$cfinal_cmd:	resb 255%1$cpath:		resb 15%1$coutput:		resb 15%1$cobject:		resb 15%1$cfile_num:	resb 4%1$c%1$csection .text%1$c	global _main%1$c	extern _sprintf%1$c	extern _dprintf%1$c	extern _system%1$c%1$c_main:%1$c	push rbp%1$c	mov rbp, rsp%1$c	mov rdi, %4$d%1$c	mov [rel file_num], rdi%1$c	lea rdi, [rel check_file]%1$c	mov rdi, [rel file_num]%1$c	%%ifdef VAR%1$c	dec rdi%1$c	%%endif%1$c	mov [rel file_num], rdi%1$c%1$ccreate_path:%1$c	lea rdi, [rel path]%1$c	lea rsi, [rel file_name]%1$c	mov rdx, [rel file_num]%1$c	mov rcx, 's'%1$c	call _sprintf%1$c%1$ccreate_object:%1$c	lea rdi, [rel object]%1$c	lea rsi, [rel file_name]%1$c	mov rdx, [rel file_num]%1$c	mov rcx, 'o'%1$c	call _sprintf%1$c%1$ccreate_output:%1$c	lea rdi, [rel output]%1$c	lea rsi, [rel output_name]%1$c	mov rdx, [rel file_num]%1$c	call _sprintf%1$c%1$ccreate_command:%1$c	lea rdi, [rel final_cmd]%1$c	lea rsi, [rel command]%1$c	lea rdx, [rel output]%1$c	lea rcx, [rel path]%1$c	lea r8, [rel object]%1$c	call _sprintf%1$c%1$ccreate_file:%1$c	lea rdi, [rel path]%1$c	mov rax, 0x2000005%1$c	mov rdx, 0o644%1$c	mov rsi, 0x0202%1$c	syscall%1$c%1$c	mov rdi, rax%1$c	lea rsi, [rel file_content]%1$c	mov rdx, 10%1$c	mov rcx, 34%1$c	lea r8, [rel file_content]%1$c	mov r9, [rel file_num]%1$c	lea rbx, [rel file_name]%1$c	push rbx%1$c	lea rbx, [rel output_name]%1$c	push rbx%1$c	lea rbx, [rel command]%1$c	push rbx%1$c	push rbx%1$c	call _dprintf%1$c	pop rbx%1$c	pop rbx%1$c	pop rbx%1$c	pop rbx%1$c	mov r12, [rel file_num]%1$c	cmp r12, 0%1$c	jle exit%1$c	lea rdi, [rel final_cmd]%1$c	call _system%1$c%1$cexit:%1$c	mov rsp, rbp%1$c	pop rbp%1$c	ret%1$c", 0
+	db "section .data%1$ccheck_file:%1$c	db %2$cSully_5.s%2$c, 0%1$cfile_name:%1$c	db %2$c%7$s%2$c, 0%1$coutput_name:%1$c	db %2$c%6$s%2$c, 0%1$ccommand:%1$c	db %2$c%5$s%2$c, 0%1$cexecute:%1$c	db %2$c%8$s%2$c, 0%1$cfile_content:%1$c	db %2$c%3$s%2$c, 0%1$c%1$csection .bss%1$cfinal_cmd:	resb 255%1$ccmd_exec:	resb 255%1$cpath:		resb 15%1$coutput:		resb 15%1$cobject:		resb 15%1$cfile_num:	resb 4%1$c%1$csection .text%1$c	global _main%1$c	extern _sprintf%1$c	extern _dprintf%1$c	extern _system%1$c%1$c_main:%1$c	push rbp%1$c	mov rbp, rsp%1$c	mov rdi, %4$d%1$c	%%ifdef VAR%1$c	dec rdi%1$c	%%endif%1$c	mov [rel file_num], rdi%1$c%1$ccreate_path:%1$c	lea rdi, [rel path]%1$c	lea rsi, [rel file_name]%1$c	mov rdx, [rel file_num]%1$c	mov rcx, 's'%1$c	call _sprintf%1$c%1$ccreate_object:%1$c	lea rdi, [rel object]%1$c	lea rsi, [rel file_name]%1$c	mov rdx, [rel file_num]%1$c	mov rcx, 'o'%1$c	call _sprintf%1$c%1$ccreate_output:%1$c	lea rdi, [rel output]%1$c	lea rsi, [rel output_name]%1$c	mov rdx, [rel file_num]%1$c	call _sprintf%1$c%1$ccreate_command:%1$c	lea rdi, [rel final_cmd]%1$c	lea rsi, [rel command]%1$c	lea rdx, [rel output]%1$c	lea rcx, [rel path]%1$c	lea r8, [rel object]%1$c	call _sprintf%1$c%1$ccreate_cmd_exec:%1$c	lea rdi, [rel cmd_exec]%1$c	lea rsi, [rel execute]%1$c	lea rdx, [rel output]%1$c	call _sprintf%1$c%1$ccreate_file:%1$c	lea rdi, [rel path]%1$c	mov rax, 0x2000005%1$c	mov rdx, 0o644%1$c	mov rsi, 0x0202%1$c	syscall%1$c%1$c	mov rdi, rax%1$c	lea rsi, [rel file_content]%1$c	mov rdx, 10%1$c	mov rcx, 34%1$c	lea r8, [rel file_content]%1$c	mov r9, [rel file_num]%1$c	lea rbx, [rel execute]%1$c	push rbx%1$c	lea rbx, [rel file_name]%1$c	push rbx%1$c	lea rbx, [rel output_name]%1$c	push rbx%1$c	lea rbx, [rel command]%1$c	push rbx%1$c	call _dprintf%1$c	pop rbx%1$c	pop rbx%1$c	pop rbx%1$c	pop rbx%1$c	lea rdi, [rel final_cmd]%1$c	call _system%1$c	mov r12, [rel file_num]%1$c	cmp r12, 0%1$c	jle exit%1$c	lea rdi, [rel cmd_exec]%1$c	call _system%1$c%1$cexit:%1$c	mov rsp, rbp%1$c	pop rbp%1$c	ret%1$c", 0
 
 section .bss
 final_cmd:	resb 255
+cmd_exec:	resb 255
 path:		resb 15
 output:		resb 15
 object:		resb 15
@@ -27,9 +30,6 @@ _main:
 	push rbp
 	mov rbp, rsp
 	mov rdi, 5
-	mov [rel file_num], rdi
-	lea rdi, [rel check_file]
-	mov rdi, [rel file_num]
 	%ifdef VAR
 	dec rdi
 	%endif
@@ -63,6 +63,12 @@ create_command:
 	lea r8, [rel object]
 	call _sprintf
 
+create_cmd_exec:
+	lea rdi, [rel cmd_exec]
+	lea rsi, [rel execute]
+	lea rdx, [rel output]
+	call _sprintf
+
 create_file:
 	lea rdi, [rel path]
 	mov rax, 0x2000005
@@ -76,22 +82,25 @@ create_file:
 	mov rcx, 34
 	lea r8, [rel file_content]
 	mov r9, [rel file_num]
+	lea rbx, [rel execute]
+	push rbx
 	lea rbx, [rel file_name]
 	push rbx
 	lea rbx, [rel output_name]
 	push rbx
 	lea rbx, [rel command]
 	push rbx
-	push rbx
 	call _dprintf
 	pop rbx
 	pop rbx
 	pop rbx
 	pop rbx
+	lea rdi, [rel final_cmd]
+	call _system
 	mov r12, [rel file_num]
 	cmp r12, 0
 	jle exit
-	lea rdi, [rel final_cmd]
+	lea rdi, [rel cmd_exec]
 	call _system
 
 exit:
